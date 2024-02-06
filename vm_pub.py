46fbf394d4b8cd2de7b49c7285c9eaa9e8305bef
victorialawton <73839142+victorialawton@users.noreply.github.com>
2024-02-06 14:55:36 -0800

    Add files via upload

---
+++ vm_pub.py
@@ -0,0 +1,49 @@
+"""EE 250L Lab 04 Starter Code
+Run vm_sub.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+from datetime import datetime
+import socket
+
+"""This function (or "callback") will be executed when this client receives 
+a connection acknowledgement packet response from the server. """
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+
+if __name__ == '__main__':
+    #get IP address
+    ip_address=0 
+    """your code here"""
+    #create a client object
+    client = mqtt.Client()
+    
+    #attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+    """Connect using the following hostname, port, and keepalive interval (in 
+    seconds). We added "host=", "port=", and "keepalive=" for illustrative 
+    purposes. You can omit this in python.
+        
+    The keepalive interval indicates when to send keepalive packets to the 
+    server in the event no messages have been published from or sent to this 
+    client. If the connection request is successful, the callback attached to
+    `client.on_connect` will be called."""
+
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+
+    """ask paho-mqtt to spawn a separate thread to handle
+    incoming and outgoing mqtt messages."""
+    client.loop_start()
+    time.sleep(1)
+
+    while True:
+        #replace user with your USC username in all subscriptions
+        client.publish("user/ipinfo", f"{ip_address}")
+        print("Publishing ip address")
+        time.sleep(4)
+
+        #get date and time 
+        """your code here"""
+        #publish date and time in their own topics
+        """your code here"""
---
+++ vm_sub.py
@@ -0,0 +1,67 @@
+"""EE 250L Lab 04 Starter Code
+Run vm_pub.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+
+"""This function (or "callback") will be executed when this client receives 
+a connection acknowledgement packet response from the server. """
+
+def on_connect(client, userdata, flags, rc):
+    """Once our client has successfully connected, it makes sense to subscribe to
+    all the topics of interest. Also, subscribing in on_connect() means that, 
+    if we lose the connection and the library reconnects for us, this callback
+    will be called again thus renewing the subscriptions"""
+
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+    #replace user with your USC username in all subscriptions
+    client.subscribe("user/ipinfo")
+    
+    #Add the custom callbacks by indicating the topic and the name of the callback handle
+    client.message_callback_add("user/ipinfo", on_message_from_ipinfo)
+
+
+"""This object (functions are objects!) serves as the default callback for 
+messages received when another node publishes a message this client is 
+subscribed to. By "default,"" we mean that this callback is called if a custom 
+callback has not been registered using paho-mqtt's message_callback_add()."""
+def on_message(client, userdata, msg):
+    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+
+#Custom message callback.
+def on_message_from_ipinfo(client, userdata, message):
+   print("Custom callback  - IP Message: "+message.payload.decode())
+
+
+
+
+if __name__ == '__main__':
+    
+    #create a client object
+    client = mqtt.Client()
+    #attach a default callback which we defined above for incoming mqtt messages
+    client.on_message = on_message
+    #attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+
+    """Connect using the following hostname, port, and keepalive interval (in 
+    seconds). We added "host=", "port=", and "keepalive=" for illustrative 
+    purposes. You can omit this in python.
+        
+    The keepalive interval indicates when to send keepalive packets to the 
+    server in the event no messages have been published from or sent to this 
+    client. If the connection request is successful, the callback attached to
+    `client.on_connect` will be called."""    
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+
+    """In our prior labs, we did not use multiple threads per se. Instead, we
+    wrote clients and servers all in separate *processes*. However, every 
+    program with networking involved generally requires multiple threads to
+    make coding simpler. Using MQTT is no different. If you are doing nothing 
+    in this thread, you can run 
+    
+    `client.loop_forever()`
+    
+    which will block forever. This function processes network traffic (socket 
+    programming is used under the hood), dispatches callbacks, and handles 
+    reconnecting."""
+    client.loop_forever()
