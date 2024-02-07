833ca6fd0622f4664fec4af73e6348b5fc92f521
Sriya Kuruppath <kuruppat@usc.edu>
2024-02-06 16:22:22 -0800

    Update vm_pub.py

--- vm_pub.py
+++ vm_pub.py
@@ -12,44 +12,44 @@ import time
 from datetime import datetime
 import socket
 
-"""This function (or "callback") will be executed when this client receives 
-a connection acknowledgement packet response from the server. """
+# This function (or "callback") will be executed when this client receives 
+# a connection acknowledgement packet response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
-
+    print("Connected to server (i.e., broker) with result code " + str(rc))
 
 if __name__ == '__main__':
-    #get IP address
-    ip_address=0 
-    """your code here"""
-    #create a client object
+    # Get IP address
+    ip_address = socket.gethostbyname(socket.gethostname())
+    
+    # Create a client object
     client = mqtt.Client()
     
-    #attach the on_connect() callback function defined above to the mqtt client
+    # Attach the on_connect() callback function defined above to the mqtt client
     client.on_connect = on_connect
-    """Connect using the following hostname, port, and keepalive interval (in 
-    seconds). We added "host=", "port=", and "keepalive=" for illustrative 
-    purposes. You can omit this in python.
-        
-    The keepalive interval indicates when to send keepalive packets to the 
-    server in the event no messages have been published from or sent to this 
-    client. If the connection request is successful, the callback attached to
-    `client.on_connect` will be called."""
-
+    
+    # Connect to the MQTT broker
     client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
-
-    """ask paho-mqtt to spawn a separate thread to handle
-    incoming and outgoing mqtt messages."""
+    
+    # Start the loop
     client.loop_start()
     time.sleep(1)
 
     while True:
-        #replace user with your USC username in all subscriptions
-        client.publish("user/ipinfo", f"{ip_address}")
-        print("Publishing ip address")
-        time.sleep(4)
+        # Publish IP address
+        client.publish("user/ipinfo", f"IP Address: {ip_address}")
+        print("Publishing IP Address")
 
-        #get date and time 
-        """your code here"""
-        #publish date and time in their own topics
-        """your code here"""
+        # Get date and time
+        now = datetime.now()
+        date_str = now.strftime("%Y-%m-%d")
+        time_str = now.strftime("%H:%M:%S")
+
+        # Publish date and time in their own topics
+        client.publish("user/date", f"Date: {date_str}")
+        print("Publishing Date:", date_str)
+
+        client.publish("user/time", f"Time: {time_str}")
+        print("Publishing Time:", time_str)
+
+        time.sleep(4)  # Wait for 4 seconds before next publish
+
